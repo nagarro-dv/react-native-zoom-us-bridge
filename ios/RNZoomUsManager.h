12664977f93b6e3cf599720dba70af076ea4f4cd
khandelwalshivam <52959169+khandelwalshivam@users.noreply.github.com>
2020-07-18 02:41:39 +0530

    Update RNZoomUsManager.h

--- ios/RNZoomUsManager.h
+++ ios/RNZoomUsManager.h
@@ -8,7 +8,7 @@ NS_ASSUME_NONNULL_BEGIN
 + (RNZoomUsManager *)sharedInstance;
 
 - (void)authenticate:(NSString *)appKey appSecret:(NSString *)appSecret completion:(void (^_Nonnull)(NSUInteger resultCode))completion;
-- (void)joinMeeting:(NSString *)meetingId userName:(NSString *)userName password:(NSString *)password completion:(void (^_Nonnull)(NSUInteger resultCode))completion;
+- (void)joinMeeting:(NSString *)meetingId userName:(NSString *)userName password:(NSString *)password meetingUrl:(NSString *)meetingUrl completion:(void (^_Nonnull)(NSUInteger resultCode))completion;
 - (void)startMeeting:(NSString *)meetingId userName:(NSString *)userName userId:(NSString *)userId userZak:(NSString *)userZak completion:(void (^_Nonnull)(NSUInteger resultCode))completion;
 
 @end
